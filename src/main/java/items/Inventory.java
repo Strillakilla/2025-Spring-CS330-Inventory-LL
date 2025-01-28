2f822cf883ef5dfbf466bd14200248696ff6c38d
Strillakilla <65351168+Strillakilla@users.noreply.github.com>
2025-01-28 13:37:39 +0900

    Update Inventory.java

--- src/main/java/items/Inventory.java
+++ src/main/java/items/Inventory.java
@@ -27,9 +27,8 @@ public class Inventory
      */
     public static void mergeStacks(ItemStack lhs, ItemStack rhs)
     {
-        // lhs needs to have items added to it.
-        // rhs's size is needed
-        // lhs.????(rhs.????)
+        lhs.addItems(rhs.size());
+        rhs.addItems(-rhs.size());
     }
 
     /**
@@ -93,8 +92,7 @@ public class Inventory
      */
     public boolean isFull()
     {
-        // Replace the next line
-        return false;
+        return this.slots.currentSize >= this.capacity;
     }
 
     /**
@@ -118,7 +116,14 @@ public class Inventory
     public ItemStack findMatchingItemStack(ItemStack key)
     {
         // Add the necessary sequential search loop
+        LinkedList.Node<ItemStack> current = this.slots.head;
 
+        while (current != null) {
+            if (current.data.equals(key))   {
+                return current.data;
+            }
+            current = current.next;
+        }
         return null;
     }
 
@@ -133,6 +138,14 @@ public class Inventory
 
         // Use the appendNode/add logic from Review 1 as your starting point
         // Once we reach this function... we know that `toAdd` must be stored
+        if (this.slots.head == null)    {
+            this.slots.head = newNode;
+            this.slots.tail = newNode;
+        }   else {
+            this.slots.tail.next = newNode;
+            this.slots.tail = newNode;
+        }
+        this.slots.currentSize++;
     }
 
     /**
@@ -188,4 +201,5 @@ public class Inventory
 
         return strBld.toString();
     }
+
 }
